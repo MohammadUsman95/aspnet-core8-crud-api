1fd64b0959627c9d847a76e50efb6922740d533d
Mohammad Usman <144525606+MohammadUsman95@users.noreply.github.com>
2025-04-13 10:01:42 +0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // constructor 1st stame click on class than ctrl. to see suggetion
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
